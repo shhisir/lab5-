5a4133c42e4139978787467f7e82741ee158ad85
shhisir <145553408+shhisir@users.noreply.github.com>
2023-10-04 22:41:55 +0545

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,28 @@
+3.Write a program to check whether a number is prime or not, with the help of Fermat’s little theorem.
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int p,status;
+	cout<<"Enter a number to check if it is prime or not : ";
+	cin>>p;
+	for(int i=3;i<=p-1;i++){
+		int aToPowerPMinus1=(pow(i,p-1));
+		if((aToPowerPMinus1)%p==1){
+			cout<<"Fermat's little theorem is true when a="<<i<<endl;
+		}
+		else{
+			cout<<"Fermat's little theorem is false when a="<<i<<endl;
+			status=1;
+		}
+	}
+	if(status==1){
+		cout<<"Fermat's little theorem proves that "<<p<<" is not a prime number"<<endl;
+	}
+	else{
+		cout<<"Fermat's little theorem proves that "<<p<<" may be a prime number"<<endl;
+	}
+	return 0;
+}
